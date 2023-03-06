b53bb5d11b4606ce32d12b2f782e3aa3a2c185af
Kesseki <zl3952@nyu.edu>
2023-03-06 14:18:35 -0500

    Black Formatted

--- users/migrations/0004_profile_bio.py
+++ users/migrations/0004_profile_bio.py
@@ -5,15 +5,14 @@ import django.utils.timezone
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('users', '0003_alter_profile_avatar'),
+        ("users", "0003_alter_profile_avatar"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='profile',
-            name='bio',
+            model_name="profile",
+            name="bio",
             field=models.TextField(default=django.utils.timezone.now),
             preserve_default=False,
         ),

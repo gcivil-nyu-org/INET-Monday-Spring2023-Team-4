b19ae08f26b0aba5decd0a8f2004e4377936c8c0
Foster Liu <90676028+zl3952@users.noreply.github.com>
2023-04-23 21:21:20 -0400

    Revert "Feature map merge filter"

--- acceptor/views.py
+++ acceptor/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-#from dashboard.models import dashboard
+from dashboard.models import dashboard
 from django.contrib.auth.decorators import login_required
+
 
 @login_required
 def acceptor(request):
-    # bins = dashboard.objects.all()
-    # context = {"bins": bins}
-    # return render(request, "acceptor/acceptor.html", context)
-    return render(request, "acceptor/acceptor.html")
-    
+    bins = dashboard.objects.all()
+    context = {"bins": bins}
+    return render(request, "acceptor/acceptor.html", context)
--- user_management/settings.py
+++ user_management/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,6 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
 
 # Application definition
 
@@ -39,7 +41,6 @@ INSTALLED_APPS = [
     "users.apps.UserConfig",
     "dashboard.apps.DashboardConfig",
     "social_django",
-    "django_filters",
     "dropoff_locator",
     "acceptor.apps.AcceptorConfig",
 ]

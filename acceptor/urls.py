30caa062d5cb72f02ff9387e97a92f8a54e78df3
Jaswanth Sai Nandipati <jn2652@nyu.edu>
2023-04-29 15:45:14 -0400

    Url Update

--- acceptor/urls.py
+++ acceptor/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import acceptor
 
 urlpatterns = [
-    path("", acceptor, name="acceptor_view"),
+    path("<int:pk>/acceptor/", acceptor, name="acceptor_view"),
 ]

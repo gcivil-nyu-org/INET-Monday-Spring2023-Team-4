4d36001ea44c069d7895a6415ddd936817f40918
Jaswanth Sai Nandipati <jn2652@nyu.edu>
2023-04-29 20:51:40 -0400

    Bug Fixes

--- acceptor/urls.py
+++ acceptor/urls.py
@@ -6,7 +6,7 @@ app_name = "acceptor"
 urlpatterns = [
     path("main/", views.dashboard, name="acceptor_view"),
     # path("new-site/", views.create_listing, name="new_site"),
-    path("<int:pk>/edit/", views.update_listing, name="edit_site"),
-    path("<int:pk>/delete/", views.delete_listing, name="delete_site"),
+    # path("<int:pk>/edit/", views.update_listing, name="edit_site"),
+    # path("<int:pk>/delete/", views.delete_listing, name="delete_site"),
     # path("map/", include("dropoff_locator.urls", namespace="dropoff_locator")),
 ]

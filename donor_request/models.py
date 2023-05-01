4fb95ca6cdda53828ca1c1c12ce22bc519068078
ap2963 <agnespark824@gmail.com>
2023-05-01 14:41:21 -0400

    merging new migrations for donor_request

---
+++ donor_request/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from users.models import Profile
+from dropoff_locator.models import Site
+from django.utils import timezone
+
+
+class Request(models.Model):
+    STATUS = [
+        ("Sent", "Sent"),  # messages not yet read by host
+        ("Open", "Open"),
+        ("Scheduled", "Scheduled"),
+        ("Rejected", "Rejected"),
+        ("Completed", "Completed"),
+        ("Canceled", "Canceled"),
+    ]
+    donor = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, unique=False, related_name="requests"
+    )
+    host = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, unique=False, related_name="receives"
+    )
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, unique=False)
+    status = models.CharField(max_length=25, choices=STATUS, default="Sent")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    read = models.BooleanField(default=False)

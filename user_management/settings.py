80364c0fa1f62cb298c2ee2c2d00a6dc50158fd8
ap2963 <agnespark824@gmail.com>
2023-05-08 10:18:50 -0400

    inbox templates

--- user_management/settings.py
+++ user_management/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#use STATICFILES_DIRS for development, STATIC_ROOT for deployment
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_DIRS = [
